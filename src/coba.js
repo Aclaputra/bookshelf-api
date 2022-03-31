5b1aa76150d5e8a1e92941d993730f6057ad7a08
Muhammad Acla <muhamadacla22@gmail.com>
2022-04-01 04:23:56 +0700

    contoh akses json

---
+++ src/coba.js
@@ -0,0 +1,21 @@
+let anjay = {
+    "status":"success",
+    "data": {
+            "books":[
+                        {
+                            "id":"khF_M3jdiRGZoD0H",
+                            "name":"How to Ace your Coding Interview",
+                            "publisher":"Gramedia Kompas"
+                        },
+                        {
+                            "id":"bXo2m7e_B-6ervm8",
+                            "name":"How to Ace your Coding Interview",
+                            "publisher":"Gramedia Kompas"
+                        }
+                    ]
+                }
+            }
+
+console.log(anjay.status);
+let books = "books"
+console.log(anjay.data.books[1]);
